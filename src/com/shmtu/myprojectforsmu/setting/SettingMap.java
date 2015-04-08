f78bb7c3a537d894e9f359b1ca93dbcb1a05cf3c
stupidlittleboy <1425211552@qq.com>
2015-04-08 21:09:47 +0800

    修改地图

--- src/com/shmtu/myprojectforsmu/setting/SettingMap.java
+++ src/com/shmtu/myprojectforsmu/setting/SettingMap.java
@@ -76,7 +76,7 @@ OnGetGeoCoderResultListener{
 		//在使用SDK各组件之前初始化context信息，传入ApplicationContext  
 		//注意该方法要再setContentView方法之前实现  
 		SDKInitializer.initialize(getApplicationContext());
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.setting_map);
 		//获取地图控件引用  
 		mMapView = (MapView) findViewById(R.id.setting_mapview);
 		mMarkerInfo = (RelativeLayout) findViewById(R.id.id_marker_info);
